7c5d3560620be69c2ebbaa340277eecf443f908a
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-10-11 18:49:04 -0500

    Updated Display

--- main.ts
+++ main.ts
@@ -1,34 +1,50 @@
 enum Keys {
+    //% block="1"
     key_1,
 
+    //% block="2"
     key_2,
 
+    //% block="3"
     key_3,
 
+    //% block="A"
     key_A,
 
+    //% block="4"
     key_4,
 
+    //% block="5"
     key_5,
 
+    //% block="6"
     key_6,
 
+    //% block="B"
     key_B,
 
+    //% block="7"
     key_7,
 
+    //% block="8"
     key_8,
 
+    //% block="9"
     key_9,
     
+    //% block="C"
     key_C,
 
+    //% block="*"
     key_MUL,
 
+    //% block="0"
     key_0,
 
+    //% block="#"
     key_HASH,
 
+    //% block="D"
     key_D
 }
 enum EnumPressed {
@@ -50,7 +66,7 @@ namespace input {
     export function isKeyPressed (key: Keys) {
         return pins.digitalReadPin(pinBinds[0][((key + 1) % 4) - 1]) + pins.digitalReadPin(pinBinds[1][key % 4]) == 2;
     }
-    //% block="when $key $pressed pressed $code";
+    //% block="when $key $pressed pressed";
     export function whenKey (key: Keys, pressed: EnumPressed, code: () => void) {
         basic.forever(function () {
             if (pressed == 0 && isKeyPressed(key)) {
